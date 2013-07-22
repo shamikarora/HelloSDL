4c03d7489c8073b025e96afdf4f18fac855a6eb3
Shamik Arora <shamikarora@gmail.com>
2013-07-22 14:47:03 -0500

    First SDL Tutorial

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include "SDL.H"
+
+int main(int argc, char* args[])
+{
+	//images
+	SDL_Surface* hello = NULL;
+	SDL_Surface* screen = NULL;
+
+	//Start SDL
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	//setup screen
+	screen = SDL_SetVideoMode(640, 480, 32, SDL_SWSURFACE);
+
+	//load image
+	hello = SDL_LoadBMP("Hello.bmp");
+
+	//apply image to the screen
+	SDL_BlitSurface(hello, NULL, screen, NULL);
+
+	//update the screen
+	SDL_Flip(screen);
+
+	//pause
+	SDL_Delay(5000);
+
+	//free the loaded image
+	SDL_FreeSurface(hello);
+
+	//Quit SDL
+	SDL_Quit();
+
+	return 0;
+}
