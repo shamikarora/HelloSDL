e8fc4f30bae9874281ac409e13fe7fc28cb5647e
Rahul Sharma <rdsharma1189@gmail.com>
2013-07-25 22:05:13 -0400

    Added background for game and set screen size.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include "SDL.H"
 
+const float ASPECT_RATIO = 16.0f / 9.0f;
+const int WINDOW_WIDTH = 1200;
+const int WINDOW_HEIGHT = ((float) WINDOW_WIDTH) / ASPECT_RATIO;
+
 int main(int argc, char* args[])
 {
 	//images
@@ -10,14 +14,14 @@ int main(int argc, char* args[])
 	SDL_Init(SDL_INIT_EVERYTHING);
 
 	//setup screen
-	screen = SDL_SetVideoMode(640, 480, 32, SDL_SWSURFACE);
+	screen = SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, 32, SDL_SWSURFACE);
 
-	//load image
-	hello = SDL_LoadBMP("Hello.bmp");
+	// Load the background image
+	hello = SDL_LoadBMP("Resources/gameBackground.bmp");
 
 	//apply image to the screen
 	SDL_BlitSurface(hello, NULL, screen, NULL);
-
+	
 	//update the screen
 	SDL_Flip(screen);
 
